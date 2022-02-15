008b07b35f88f85d31eada511e15b85b62730492
AntonyMorgante <antonymorgante1@gmail.com>
2022-02-15 15:08:38 +0100

    snack B

---
+++ snackB.php
@@ -0,0 +1,34 @@
+<?php
+    $name = $_GET["name"];
+    $mail= $_GET["mail"];
+    $age= $_GET["age"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (strlen($name)>3){
+            if (is_numeric($age)){
+                if ((strpos($mail, '.') !== false) && (strpos($mail, '@') !== false)){
+                    echo "Accesso riuscito!";
+                }
+                else {
+                    echo "Accesso negato!";
+                }
+            }
+            else {
+                echo "Accesso negato!";
+            }
+        } else {
+            echo "Accesso negato!";
+        }
+    ?>
+</body>
+</html>
